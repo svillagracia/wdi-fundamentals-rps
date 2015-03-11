40261f05c4dc086f7d92d650788fff3c5fd078d3
svillagracia <svillagracia@outlook.com>
2015-03-10 18:43:16 -0700

    added move variable, changed return

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,16 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return = move || getInput();
+    move = move || getInput();
+    return getPlayerMove;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return = move || randomPlay();
+    move = move || randomPlay();
+    return getComputerMove;
 }
 
 function getWinner(playerMove,computerMove) {
